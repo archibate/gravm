f070d2bef4ad39f9bb4d147151f33ff81898e933
archibate <m17721388340@163.com>
2018-01-18 11:38:20 +0800

    Create world.js

---
+++ world.js
@@ -0,0 +1,25 @@
+var world = new Array();
+
+function coolFunction(distant) {
+ var distanceSquared = distant.dotProduct(distant);
+ if (distanceSquared <= 0.00001)
+  return zeroVector();
+ var distanceCubed = distanceSquared * Math.sqrt(distanceSquared);
+ return distant.divide(distanceCubed);
+}
+
+function getFieldAt(position, excluded) {
+ var acceleration = zeroVector();
+ for (var i in world) {
+  if (i === excluded)
+   continue;
+  var other = world[i];
+  if (other.mass === 0)
+   continue;
+  var distant = other.position.subtract(position);
+  var accOnce = coolFunction(distant);
+  accOnce.multiplyUp(other.mass);
+  acceleration.addUp(accOnce);
+ }
+ return acceleration;
+}
