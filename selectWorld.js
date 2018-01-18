6182e948ce87f5e55cde9f4695f0da295a870692
archibate <m17721388340@163.com>
2018-01-18 11:39:51 +0800

    Update selectWorld.js

--- selectWorld.js
+++ selectWorld.js
@@ -6,7 +6,6 @@ world[2] = new Ball(new Vector(0, 20, 0), new Vector(-4.5, 0, 0), 0);
 }
 
 function world2() {
-worldOnce = true;
 world[0] = new Ball(new Vector(18, 0, 0), new Vector(0, 0, 0), 1000);
 world[1] = new Ball(new Vector(-18, 0, 0), new Vector(0, 0, 0), -250);
 }
@@ -18,7 +17,6 @@ world[2] = new Ball(new Vector(2, 18, 0), new Vector(-9, 13, 0), 0);
 }
 
 function world4() {
-worldOnce = true;
 world[0] = new Ball(new Vector(20, 0, 0), new Vector(0, 0, 0), 800);
 world[1] = new Ball(new Vector(-20, 0, 0), new Vector(0, 0, 0), -800);
 }
