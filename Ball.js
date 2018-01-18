4d0c07b22c7316400d9a563ba7398f87c1612c16
archibate <m17721388340@163.com>
2018-01-18 11:52:33 +0800

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -19,20 +19,3 @@ Ball.prototype = {
   this.velocity.addUp(acceleration.multiply(timePassed));
  },
 };
-
-    </script><script>
-
-function fillCircle(ctx, x, y, r) {
- ctx.beginPath();
- ctx.arc(x, y, r, 0, 2 * Math.PI);
- ctx.fill();
-}
-
-function drawArrow(ctx, x0, y0, dx, dy) {
- ctx.moveTo(x0, y0);
- ctx.lineTo(x0 + dx, y0 + dy);
- ctx.lineTo(x0 + 0.8 * dx - 0.2 * dy, y0 + 0.8 * dy + 0.2 * dx);
- ctx.moveTo(x0 + dx, y0 + dy);
- ctx.lineTo(x0 + 0.8 * dx + 0.2 * dy, y0 + 0.8 * dy - 0.2 * dx);
- ctx.stroke();
-}
