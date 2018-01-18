5770ac65f55f636736d708fee5a58dfadacb7abb
archibate <m17721388340@163.com>
2018-01-18 12:00:56 +0000

    Update save.js

--- save.js
+++ save.js
@@ -11,8 +11,23 @@ function saveBall(ball) {
 }
 
 function saveWorld(world) {
+  var res = "";
   for (var i in world) {
     res += saveBall(world[i]) + "\n";
   }
   return res;
+}
+
+function loadBall(str) {
+  var list = str.split(",");
+  return new Ball(new Vector([list[0], list[1], list[2]), new Vector([list[3], list[4], list[5]), list[6]);
+}
+
+function loadWorld(str) {
+  var list = str.split("\n");
+  var world = new Array(list.length);
+  for (var i in list) {
+    world[i] = loadBall(list[i]);
+  }
+  return world;
 }
