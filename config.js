58e342be4b8166b6458e8f3b393e105ddfb76797
archibate <m17721388340@163.com>
2018-01-24 14:35:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var accType = 'elec';
+var accType = 'grav';
 var world = new Array(); // TODO: why here?
 var paused = false; // TODO: to put iin state.js might be better
 var needRenderField = false;
