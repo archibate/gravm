daf5d0df124f748029e61a9ceec14b23a69a9e18
archibate <m17721388340@163.com>
2018-01-24 14:33:43 +0000

    Update dte.js

--- dte.js
+++ dte.js
@@ -3,6 +3,9 @@ function outOfScreen(position) {
  return xy.x > screenX || xy.x < 0 || xy.y < 0 || xy.y > screenY;
 }
 
+var rocket = new Ball(zeroVector(), zeroVector(), 0, 0);
+rocket.color = '#FFFFFF';
+
 function dteMain() {
  if (paused)
   return;
@@ -11,6 +14,10 @@ function dteMain() {
  ctx.fillRect(0, 0, screenX, screenY);
 
  timePassed = dteInterval;
+ 
+ rocket.render(ctx);
+ var rokAcc = getFieldAt(rocket.position);
+ rocket.accByTime(timePassed, rokAcc);
 
  for (var i in world) {
   var ball = world[i];
