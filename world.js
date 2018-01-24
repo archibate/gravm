cf19e239b9bf816a9c988cda802de02cfeb447be
archibate <m17721388340@163.com>
2018-01-24 05:37:00 +0000

    Update world.js

--- world.js
+++ world.js
@@ -18,7 +18,7 @@ function getFieldAt(position, excluded) {
    continue;
   var distant = other.position.subtract(position);
   var accOnce = coolFunction(distant);
-  accOnce.multiplyUp(other.getForceScaler());
+  accOnce.multiplyUp(other);
   acceleration.addUp(accOnce);
  }
  return acceleration;
