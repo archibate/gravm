04924f1b7cf6b0708c1b75fb2c1e36c2e6d90394
archibate <m17721388340@163.com>
2018-01-24 05:36:48 +0000

    Update world.js

--- world.js
+++ world.js
@@ -1,15 +1,5 @@
 var world = new Array();
 
-function getForceScalerElec() {
- return this.mass / this.quantity;
-}
-
-function getForceScalerGrav() {
- return -this.mass;
-}
-
-Ball.prototype.getForceScaler = getForceScalerGrav; // changes here
-
 function coolFunction(distant) {
  var distanceSquared = distant.dotProduct(distant);
  if (distanceSquared <= 0.00001)
@@ -26,7 +16,7 @@ function getFieldAt(position, excluded) {
   var other = world[i];
   if (other.mass === 0)
    continue;
-  var distant = position.subtract(other.position);
+  var distant = other.position.subtract(position);
   var accOnce = coolFunction(distant);
   accOnce.multiplyUp(other.getForceScaler());
   acceleration.addUp(accOnce);
