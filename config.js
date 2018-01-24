f7e18f4bb06260cfe61a8a6caee470897eaa1407
archibate <m17721388340@163.com>
2018-01-24 07:03:19 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
 var accType = 'elec';
+var paused = false; // TODO: state.js might be better
+var needRenderField = false;
+var dteInterval = 0.02;
+var renderFieldInterval = 0.1;
+var renderFieldClock = 0;
